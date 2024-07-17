b0b17199c01faaab03fa12f7a27aba51b52a89a5
Skye Pooley <tpb6375@autuni.ac.nz>
2024-07-17 20:05:30 +1200

    Sorting Key Enum
    ImageProcessor.ContrastSort now takes a parameter for the colour attribute to sort pixels by.
    Hue, Saturation, and Luminance are allowed.
    Argument is take as an enum.
    Also added method comments.

--- ImageManipulationSystemDrawing/ImageProcessor.cs
+++ ImageManipulationSystemDrawing/ImageProcessor.cs
@@ -1,56 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-#pragma warning disable CA1416
 
 namespace ImageManipulationSystemDrawing
 {
     internal class ImageProcessor
     {
         private Bitmap Image { get; }
+        public enum SortingKey {
+            Hue,
+            Saturation,
+            Luminance
+        }
 
-        public ImageProcessor(String path)
+        /// <summary>
+        /// Construct an image processor using an image loaded from disk.
+        /// </summary>
+        /// <param name="path">Path to the image on file.</param>
+        public ImageProcessor(string path)
         {
             Image = new Bitmap(path);
             Console.WriteLine("Width: " + Image.Width);
             Console.WriteLine("Height: " + Image.Height);
         }
 
+        /// <summary>
+        /// Construct an image processor using an existing Bitmap object.
+        /// </summary>
+        /// <param name="image">System.Drawing.Bitmap object containing a preloaded image.</param>
         public ImageProcessor(Bitmap image)
         {
             this.Image = (Bitmap) image.Clone();
         }
 
-        public void SaveTo(String path)
+        /// <summary>
+        /// Saves the current state of the contained bitmap to disk.
+        /// </summary>
+        /// <param name="path">Save location for resulting image.</param>
+        public void SaveTo(string path)
         {
             Image.Save(path);
         }
-
-        public void Divide(int division)
-        {
-            for (int row = 0; row < Image.Height; row++)
-            {
-                for (int col = 0; col < Image.Width; col++)
-                {
-                    Color pixel = Image.GetPixel(col, row);
-                    Color newPixel = this.Divide(pixel, division);
-                    Image.SetPixel(col, row, newPixel);
-                }
-            }
-        }
-
-        private Color Divide(Color pixel, int division)
-        {
-            int red = pixel.R / 2;
-            int green = pixel.G / 2;
-            int blue = pixel.B / 2;
-            return Color.FromArgb(red, green, blue);
-        }
         
+        /// <summary>
+        /// Creates a black and white contrast mask from the contained bitmap.
+        /// Pixels greater than the given brightness threshold will be set to white, all others will be black.
+        /// This method is destructive, the result will overwrite the current bitmap.
+        /// </summary>
+        /// <param name="threshold">Brightness threshold for contrast mask. Must be between 0.0 (black) and 1.0 (white)</param>
         public void ContrastMask(float threshold)
         {
             for (int row = 0; row < Image.Height; row++)
@@ -63,7 +62,7 @@ namespace ImageManipulationSystemDrawing
                 }
             }
         }
-
+        
         private Color ContrastMask(Color pixel, float threshold)
         {
             float brightness = pixel.GetBrightness();
@@ -77,6 +76,9 @@ namespace ImageManipulationSystemDrawing
             }
         }
 
+        /// <summary>
+        /// Sorts all pixels in all rows of the image by their brightness.
+        /// </summary>
         public void SortAll()
         {
             for (int row = 0; row < Image.Height; row++)
@@ -99,7 +101,7 @@ namespace ImageManipulationSystemDrawing
             }
         }
 
-        public void ContrastSort(float threshold)
+        public void ContrastSort(float threshold, SortingKey sortBy)
         {
             // Generate a contrast mask of the image for span generation.
             Console.WriteLine("Generating Contrast Mask");
@@ -140,9 +142,15 @@ namespace ImageManipulationSystemDrawing
             Console.WriteLine("Sorting Spans");
             foreach (Span span in spans)
             {
-                // span.HueSort();
-                // span.SaturationSort();
-                span.LuminanceSort();
+                // Sort based on caller determined attribute
+                switch (sortBy)
+                {
+                    case SortingKey.Hue: span.HueSort(); break;
+                    case SortingKey.Saturation: span.SaturationSort(); break;
+                    case SortingKey.Luminance: span.LuminanceSort(); break;
+                }
+
+                //Place sorted pixels back into the image
                 (int x, int y) position = span.StartPosition;
                 foreach (Color pixel in span.Pixels)
                 {
--- ImageManipulationSystemDrawing/Program.cs
+++ ImageManipulationSystemDrawing/Program.cs
@@ -9,13 +9,12 @@ namespace ImageManipulationSystemDrawing
         {
             try
             {
-                var processor = new ImageProcessor(@"C:\Users\wmpoo\OneDrive\Programs\ImageManipulationSystemDrawing\ImageManipulationSystemDrawing\Properties\demoSmall.png");
-                // processor.ContrastMask(0.4f);
-                // processor.SortAll();
-                processor.ContrastSort(0.3f);
-                processor.SaveTo(@"C:\Users\wmpoo\OneDrive\Programs\ImageManipulationSystemDrawing\ImageManipulationSystemDrawing\output.png");
+                var imageProcessor = new ImageProcessor(@"C:\Users\wmpoo\OneDrive\Programs\ImageManipulationSystemDrawing\ImageManipulationSystemDrawing\Properties\demoSmall.png");
+                //imageProcessor.ContrastMask(0.6f);
+                imageProcessor.ContrastSort(0.6f, ImageProcessor.SortingKey.Saturation);
+                imageProcessor.SaveTo(@"C:\Users\wmpoo\OneDrive\Programs\ImageManipulationSystemDrawing\ImageManipulationSystemDrawing\output.png");
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
